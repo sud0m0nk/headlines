63dacd5a47d8fd1e6e72884941958159f1385206
sud0m0nk <parresagar@gmail.com>
2019-06-08 14:21:52 +0530

    Add weather

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib2
+import urllib
 
 app = Flask(__name__)
 
@@ -10,18 +13,49 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
              'fox': 'http://feeds.foxnews.com/foxnews/latest',
              'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
+WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=f5101e00abb59860ec6d2caab5fb0051"
+
+DEFAULTS = {'publication':'bbc',
+            'city': 'London,UK'}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
-	query = request.args.get("publication")
+def home():
+	# get customized headlines, based on user input or default
+	publication = request.args.get('publication')
+	if not publication:
+		publication = DEFAULTS['publication']
+	articles = get_news(publication)
+	# get customized weather based on user input or default
+	city = request.args.get('city')
+	if not city:
+	    city = DEFAULTS['city']
+	weather = get_weather(city)
+	return render_template("home.html",articles=articles,weather=weather)	
+
+
+def get_news(query):
 	if not query or query.lower() not in RSS_FEEDS:
 		publication = "bbc"
 	else:
 	    publication = query.lower()	
 
 	feed = feedparser.parse(RSS_FEEDS[publication])
-	return render_template("home.html",articles=feed['entries'])
+	return feed['entries']
+
+def get_weather(query): 
+	query = urllib.quote(query)
+	url = WEATHER_URL.format(query)
+	data = urllib2.urlopen(url).read()
+	parsed = json.loads(data)
+	weather = None
+	if parsed.get("weather"):
+		weather = {"desciption":parsed["weather"][0]["description"],
+		           "temperature":parsed["main"]["temp"],
+		           "city":parsed["name"],
+		           "country":parsed["sys"]["country"]
+		           }
+	return weather	           
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
